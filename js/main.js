bbfc8a2130d69d6cb57bea7cfcc43c3361b23341
Ryan Magoon <mr.magoon5@gmail.com>
2017-01-24 13:56:35 -0500

    finished the meat of the app

--- js/main.js
+++ js/main.js
@@ -9,8 +9,64 @@ $(document).ready(() => {
         client_secret: '60501c0ac69f7c2702edc599579f713497150799',
       },
     }).done((user) => {
+      $.ajax({
+        url: `https://api.github.com/users/${username}/repos`,
+        data: {
+          client_id: 'b11ca1439eddd99be904',
+          client_secret: '60501c0ac69f7c2702edc599579f713497150799',
+          sort: 'created: asc',
+          per_page: 5,
+        },
+      }).done((repos) => {
+        $.each(repos, (index, repo) => {
+          $('#repos').append(`
+            <div class="well">
+              <div class="row">
+                <div class="col-md-7">
+                  <strong>${repo.name}</strong>: ${repo.description}
+                </div>
+                <div class="col-md-3">
+                  <span class="label label-default">Forks: ${repo.forks_count}</span>
+                  <span class="label label-danger">Watchers: ${repo.watchers_count}</span>
+                  <span class="label label-success">Stars: ${repo.stargazers_count}</span>
+                </div>
+                <div class="col-md-2">
+                  <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+                </div>
+              </div>
+            </div>
+          `)
+        })
+      })
       $('#profile').html(`
-          ${user.name}
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">${user.name}</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-md-3">
+                <img class="thumbnail avatar" src="${user.avatar_url}" alt="" />
+                <a class="btn btn-primary btn-block" target="_blank" href="${user.html_url}">View Profile</a>
+              </div>
+              <div class="col-md-9">
+                <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                <span class="label label-success">Followers: ${user.followers}</span>
+                <span class="label label-info">Following: ${user.following}</span>
+                <br /><br />
+                <ul class="list-group">
+                  <li class="list-group-item">Company: ${user.company}</li>
+                  <li class="list-group-item">Blog: ${user.blog}</li>
+                  <li class="list-group-item">Location: ${user.location}</li>
+                  <li class="list-group-item">Member Since: ${user.created_at}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
         `)
     })
   })
